598cf302d4d132291e48338115431410eacd0761
ZHURENJIE <ZHURENJIE@qq.com>
2021-10-24 19:50:39 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author ZhuRenjie
+ * @create 2021-10-24 19:41
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
+
